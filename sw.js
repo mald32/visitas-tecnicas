3339511d4c918c7c42049464b86b304e821dc7e0
Miguel Leon <66572045+mald32@users.noreply.github.com>
2026-09-09 00:20:22 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "visitas-tecnicas-v1";
+const ARCHIVOS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./config.js",
+  "./db.js",
+  "./graph.js",
+  "./informes.js",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) => Promise.all(nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return; // Graph, login de Microsoft y MSAL van directo a la red
+
+  event.respondWith(caches.match(event.request).then((cacheada) => cacheada || fetch(event.request)));
+});
